4c9518380b863acde12934910e2b66973b3b46d9
Bobby Rehm <Bobby@Bobbys-MacBook.local>
2014-05-03 02:10:49 -0400

    Initial Commit

---
+++ Family Foods Grocers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Family Foods Grocers
+//
+//  Created by Bobby Rehm on 5/3/14.
+//  Copyright (c) 2014 Bobby Rehm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
